54a32896f6c52a1b293bf6d7358054de386737b8
jhalto <zobayerarmannadim@gmail.com>
2024-12-29 17:21:51 +0600

    Complete

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:sm_task/widgets/categories_widgets.dart';
 import 'package:sm_task/widgets/common_widgets.dart';
 import 'package:sm_task/widgets/continue_watching_widgets.dart';
+import 'package:sm_task/widgets/custom_nav_bar.dart';
+import 'package:sm_task/widgets/recommend_widgets.dart';
 import 'package:sm_task/widgets/searchBar.dart';
 import 'package:sm_task/widgets/suggestion_widget.dart';
 import 'package:sm_task/widgets/trending_widgets.dart';
@@ -16,144 +18,152 @@ class Home extends StatelessWidget {
      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
      return Scaffold(
-
+             
 
       backgroundColor: Color(0xff11161F),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16,right: 16,top: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                       children: [
-                         SizedBox(
-                             width: MediaQuery.of(context).size.width*.70,
+      body: SingleChildScrollView(
 
-                             child: RichText(text: TextSpan(
-                               children: [
-                                 TextSpan(
-                                   text: "Hello ",
-                                   style: appStyle(20,white,FontWeight.bold),
-                                 ),
-
-                                 TextSpan(
-                                   text: 'Rifat',
-                                   style: appStyle(20,white)
-                                 )
-                               ]
-                             )),
-                         ),
-                         Text("Let's watch today",style: appStyle(14,white.withOpacity(.50)),)
-                       ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16,right: 16,top: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           SizedBox(
+                               width: MediaQuery.of(context).size.width*.70,
+        
+                               child: RichText(text: TextSpan(
+                                 children: [
+                                   TextSpan(
+                                     text: "Hello ",
+                                     style: appStyle(20,white,FontWeight.bold),
+                                   ),
+        
+                                   TextSpan(
+                                     text: 'Rifat',
+                                     style: appStyle(20,white)
+                                   )
+                                 ]
+                               )),
+                           ),
+                           Text("Let's watch today",style: appStyle(14,white.withOpacity(.50)),)
+                         ],
+                      ),
+        
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 46,
+                        width: 46,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+        
+                          image: DecorationImage(image: AssetImage("lib/images/user.png",),fit: BoxFit.fill)
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                 SearchBarStyle(),
+                  SizedBox(width: 10,),
+                  Container(
+                    width: 43,
+                    height: 43,
+        
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7),
+                      color: Color(0xffFF3440),
+        
                     ),
-
+                    child: Center(
+                      child: Container(
+                        height: 24,
+                        width: 24,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage("lib/images/Settings-adjust.png"))
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.only(left: 2.0,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Categories",style: appStyleAkatab(18,white,FontWeight.w500),),
+                    Text("See More",style: appStyle(14,white),)
                   ],
                 ),
-                Row(
+              ),
+              SizedBox(height: 10,),
+              Categories(),
+              SizedBox(height: 20,),
+              SuggestionWidget(),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.only(left: 2.0,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      height: 46,
-                      width: 46,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-
-                        image: DecorationImage(image: AssetImage("lib/images/user.png",),fit: BoxFit.fill)
-                      ),
-                    )
+                    Text("Trending Movies",style: appStyleAkatab(18,white,FontWeight.w500),),
+                    Text("See More",style: appStyle(14,white),)
                   ],
-                )
-              ],
-            ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-               SearchBarStyle(),
-                SizedBox(width: 10,),
-                Container(
-                  width: 43,
-                  height: 43,
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: Color(0xffFF3440),
-
-                  ),
-                  child: Center(
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(image: AssetImage("lib/images/Settings-adjust.png"))
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(left: 2.0,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Categories",style: appStyleAkatab(18,white,FontWeight.w500),),
-                  Text("See More",style: appStyle(14,white),)
-                ],
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            Categories(),
-            SizedBox(height: 20,),
-            SuggestionWidget(),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(left: 2.0,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Trending Movies",style: appStyleAkatab(18,white,FontWeight.w500),),
-                  Text("See More",style: appStyle(14,white),)
-                ],
+              SizedBox(height: 20,),
+              TrendingWidgets(),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.only(left: 2.0,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Continue Watching",style: appStyleAkatab(18,white,FontWeight.w500),),
+                    Text("See More",style: appStyle(14,white),)
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            TrendingWidgets(),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(left: 2.0,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Continue Watching",style: appStyleAkatab(18,white,FontWeight.w500),),
-                  Text("See More",style: appStyle(14,white),)
-                ],
+              SizedBox(height: 16,),
+              ContinueWatchingWidget(),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.only(left: 2.0,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Recommend For You",style: appStyleAkatab(18,white,FontWeight.w500),),
+                    Text("See More",style: appStyle(14,white),)
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16,),
-            ContinueWatchingWidget(),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(left: 2.0,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Recommend For You",style: appStyleAkatab(18,white,FontWeight.w500),),
-                  Text("See More",style: appStyle(14,white),)
-                ],
-              ),
-            ),
+              SizedBox(height: 16,),
+              RecommendWidgets(),
+              SizedBox(height: 200,)
+            ],
 
-          ],
-
+          ),
         ),
       ),
+       bottomNavigationBar: CustomNavBar(
+
+       ),
     );
   }
 }
